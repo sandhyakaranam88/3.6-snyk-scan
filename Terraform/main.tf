288856fb9bd2d5738c5927109694db44b7367cf7
Sandhya <111405460+sandhyakaranam88@users.noreply.github.com>
2025-07-26 19:30:05 +0800

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -87,7 +87,10 @@ resource "aws_ecs_task_definition" "queue" {
   family = "${var.prefix}-queue-task"
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu = "256"; memory = "512"; execution_role_arn = aws_iam_role.task_exec.arn
+  cpu                   = "256"
+  memory                = "512"
+  execution_role_arn    = aws_iam_role.task_exec.arn
+
   container_definitions = jsonencode([{
     name="queue"; image="${aws_ecr_repository.queue.repository_url}:${var.image_tag}"
     essential=true; portMappings=[{containerPort=5001,hostPort=5001}]
